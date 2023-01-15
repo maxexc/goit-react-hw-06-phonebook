889bac1c415aab8bb9e8de7bd4291e98c1e750db
Maksym Solomko <max.exc7@gmail.com>
2023-01-15 22:18:12 +0200

    test

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -3,8 +3,11 @@ import {
   FilterTitle, 
   Filterinput, 
 } from './Filter.styled';
+import {useSelector, useDispatch} from 'react-redux';
 
 const Filter = ({value, onFilter}) => {
+    const valueRedux = useSelector(state => state.myValue);
+    console.log(valueRedux);
     return (       
         <label>
             <FilterTitle>Filter</FilterTitle>  
@@ -15,6 +18,9 @@ const Filter = ({value, onFilter}) => {
                 onChange={onFilter}
                 placeholder="Enter request"
             ></Filterinput>
+            <div>
+                <nav>{valueRedux}</nav>
+            </div>
         </label>     
     )    
  }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createReducer, createAction } from '@reduxjs/toolkit'
+
+const increment = createAction('myValue/increment');
+const decrement = createAction('myValue/increment');
+
+console.log(increment.toString());
+
+
+const myReducer = createReducer(100, {
+  [increment]: (state, action) => state + action.payload,
+  [decrement]: (state, action) => state - action.payload,
+});
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    myValue: myReducer,
+  },
 })
