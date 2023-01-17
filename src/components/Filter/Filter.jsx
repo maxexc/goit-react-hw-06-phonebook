34b898b03a1a572990b9a9a26ff7d2eea4addc6d
Maksym Solomko <max.exc7@gmail.com>
2023-01-17 15:25:50 +0200

    update

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -3,15 +3,10 @@ import {
   FilterTitle, 
   Filterinput, 
 } from './Filter.styled';
-import {useSelector, useDispatch} from 'react-redux';
-import { decrement, increment } from 'redux/store';
-// import { increment, decrement} from '../../redux/store'
 
 
 const Filter = ({value, onFilter}) => {
-    const dispatch = useDispatch();
-    const valueRedux = useSelector(state => state.myValue);
-    console.log(valueRedux);
+    
     return (       
         <label>
             <FilterTitle>Filter</FilterTitle>  
@@ -21,14 +16,7 @@ const Filter = ({value, onFilter}) => {
                 value={value}
                 onChange={onFilter}
                 placeholder="Enter request"
-            ></Filterinput>
-            <div>
-                <nav>
-                    {valueRedux}
-                    <button onClick={() => dispatch(increment(100))}>Increment</button>
-                    <button onClick={() => dispatch(decrement(50))}>Decrement</button>
-                </nav>
-            </div>
+            ></Filterinput>            
         </label>     
     )    
  }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,74 +1,37 @@
-import { configureStore  } from '@reduxjs/toolkit'
-import { createReducer, createAction, createSlice } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage';
+import {
+  // persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import { filtersReducer } from './filterSlice';
 
-// make slice
-const myValueSlice = createSlice({
-  name: "myValue", 
-  // initialState: {value: 100}, 
-  initialState: 150, 
-  reducers: {
-    increment(state, action) {
-      return state + action.payload
-      // state.value += action.payload;
-    },
-    decrement(state, action) {
-      return state - action.payload;
-    },
-  }
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['filter'],
+};
+
+
+const rootReducer = combineReducers({
+  // contacts: contactsReducer,
+  filter: filtersReducer,
 });
 
-console.log(myValueSlice);
-
-export const { increment, decrement } = myValueSlice.actions;
-
-const itemsSlice = createSlice({
-  name: 'item',
-  initialState: [],
-  reducers: {
-    add(state, action) {
-      state.push(action.payload)
-    },
-    remove(state, action) {
-      return state.filter(item => item.id !== action.payload);
-    },
-  },
-})
-
-export const {add, remove} = itemsSlice.actions;
-
-// export const increment = createAction('myValue/increment');
-// export const decrement = createAction('myValue/decrement');
-
-// console.log(increment.toString());
-
-
-// const myReducer = createReducer(100, {
-//   [increment]: (state, action) => state + action.payload,
-//   [decrement]: (state, action) => state - action.payload,
-// });
-
-// 
-// export const add = createAction('items/add');
-// export const remove = createAction('items/remove')
-// // IMMER
-// const itemsReducer = createReducer([], {
-//   // [add]: (state, action) => [...state, action.payload,],
-//   [add]: (state, action) => state.push(action.payload),
-//   [remove]: (state, action) => state.filter(item => item.id !== action.payload),
-// });
-// // 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    myValue: myValueSlice.reducer,
-    items: itemsSlice.reducer,
-  },
+  reducer: persistedReducer,
+    middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
-
-// export const store = configureStore({
-//   reducer: {
-//     myValue: myReducer,
-//     // 
-//     items: itemsReducer,
-//   },
-// })
